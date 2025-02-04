d8accca132ffc5babcc4d1d5bd3c693fb365b544
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2025-02-04 08:16:17 -0300

    Teste da criação de usuário no domínio

---
+++ AutenticacaoDoisFatores/AutenticacaoDoisFatores.Testes/Dominio/Dominios/DominioDeUsuariosTeste.cs
@@ -0,0 +1,36 @@
+﻿using AutenticacaoDoisFatores.Dominio.Dominios;
+using AutenticacaoDoisFatores.Dominio.Repositorios;
+using AutenticacaoDoisFatores.Testes.Compartilhados;
+using Moq;
+using Moq.AutoMock;
+
+namespace AutenticacaoDoisFatores.Testes.Dominio.Dominios
+{
+    public class DominioDeUsuariosTeste
+    {
+        [Fact]
+        internal async Task DeveCriarUsuario()
+        {
+            #region Preparação do teste
+
+            var mocker = new AutoMocker();
+
+            var dominio = mocker.CreateInstance<DominioDeUsuario>();
+
+            var usuarioParaCriar = ConstrutorDeUsuariosTeste
+                .RetornarConstrutorDeNovo()
+                .ConstruirNovo();
+
+            #endregion
+
+            await dominio.CriarUsuarioAsync(usuarioParaCriar);
+
+            #region Verificação do teste
+
+            mocker.Verify<IRepositorioDeUsuarios>(r => r.Adicionar(usuarioParaCriar), Times.Once);
+            mocker.Verify<IRepositorioDeUsuarios>(r => r.SalvarAlteracoesAsync(), Times.Once);
+
+            #endregion
+        }
+    }
+}
